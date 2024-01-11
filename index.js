d66dfee10c97bcfc707b778306b7523fb28bbac2
Akshat Jain <akshatjain0963@gmail.com>
2024-01-11 15:41:03 +0530

    sahi he

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-
+const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hellllo World!");
 });
 app.get("/test", (req, res) => {
   res.send("<h1>This is test page</h1>");
 });
-
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
+console.log("checking");
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
